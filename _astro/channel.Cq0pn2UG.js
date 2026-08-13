11e61fbb03df273b2d4ce31232a00a0c31e0a232
Azure CI <ci@runfoodapp.com>
2026-08-13 15:39:52 +0000

    docs: deploy 2177e647dfbc9e80f3aaa6573f826bad00451c8a

---
+++ _astro/channel.Cq0pn2UG.js
@@ -0,0 +1,1 @@
+import{at as e,ot as t}from"./chunk-WYO6CB5R.Dul1w_Ir.js";var n=(n,r)=>t.lang.round(e.parse(n)[r]);export{n as t};
---
+++ _astro/swimlanesDiagram-G3AALYLV.qXoXBdbH.js
@@ -0,0 +1,8 @@
+import"./src.OgxTg4hg.js";import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./chunk-WYO6CB5R.Dul1w_Ir.js";import"./chunk-ICXQ74PX.DvvEyLlD.js";import"./chunk-HOUHSVGY.BpZiylp2.js";import"./chunk-Q4XR5HBZ.C16inzZG.js";import"./chunk-7BUUIJ7U.CAhmNhj7.js";import"./chunk-OGEWGWER.D_XhdtjN.js";import"./chunk-32BRIVSS.DPGaWpVq.js";import"./chunk-XXDRQBXY.B0iYp7hA.js";import"./chunk-VR4S4FIN.B_b8VzZJ.js";import"./chunk-C7G6YPKG.CJRvbr_X.js";import"./chunk-ZGVPDNZ5.bRtq7t0b.js";import"./chunk-52WLFC77.UQ7S62bC.js";import"./chunk-FWX5IMBZ.tQ6dX0Mf.js";import"./chunk-ZIRB5QZD.Dh20_f-h.js";import{r as t,t as n}from"./chunk-PUDLZKDR.DN6_bYO8.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
